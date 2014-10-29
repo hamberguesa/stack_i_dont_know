c98d225e7bf340a8f6a3e14ff001c69d1c892c3b
Amber H. & Nelly Lam <pair+hamberguesa.nellylam@devbootcamp.com>
2014-10-29 16:09:08 -0700

    New Question form

--- stack_i_dont_know/app/controllers/questions_controller.rb
+++ stack_i_dont_know/app/controllers/questions_controller.rb
@@ -6,4 +6,22 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.save
+
+    redirect_to @question
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content)
+  end
+
 end
