00852becc896dbd52774767c98a001c13794c817
Amber H. & Nelly Lam <pair+hamberguesa.nellylam@devbootcamp.com>
2014-10-29 11:09:45 -0700

    add question specs, rails_helper and spec_helper

--- stack_i_dont_know/spec/models/question_spec.rb
+++ stack_i_dont_know/spec/models/question_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Question, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Question do
+  before(:each) do
+    @question = Question.new(title: "what?", content:"how?")
+  end
+  it 'has a title' do
+    expect(@question.title).to eq "what?"
+  end
+  it 'has content' do
+    expect(@question.content).to eq "how?"
+  end
+
+  after(:each) do
+    @question = nil
+  end
 end
+
