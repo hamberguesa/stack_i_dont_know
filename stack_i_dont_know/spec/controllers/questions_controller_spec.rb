66210c737729cd9fe1ef8372d6cf247c0a47b425
Amber Hager <ambermhager@gmail.com>
2014-10-29 21:42:28 -0700

    complete questions controller specs

--- stack_i_dont_know/spec/controllers/questions_controller_spec.rb
+++ stack_i_dont_know/spec/controllers/questions_controller_spec.rb
@@ -17,7 +17,7 @@ describe QuestionsController do
       expect(response).to render_template :index
     end
 
-    it "routes /questions to messages#index" do
+    it "routes /questions to questions#index" do
       expect(get: "/questions").to route_to(
         controller: "questions",
         action: "index"
@@ -36,7 +36,7 @@ describe QuestionsController do
       expect(response).to render_template :show, id: @question.id
     end
 
-    it "routes /questions/:id to messages#show" do
+    it "routes /questions/:id to questions#show" do
       expect(get: "/questions/#{@question.id}").to route_to(
         controller: "questions",
         action: "show",
@@ -46,7 +46,22 @@ describe QuestionsController do
   end
 
   context "NEW" do
+    it "new is successful" do
+      get :new
+      expect(response.status).to eq 200
+    end
 
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "routes /questions/new to questions#new" do
+      expect(get: "/questions/new").to route_to(
+        controller: "questions",
+        action: "new"
+        )
+    end
   end
 
   context "CREATE" do
@@ -68,15 +83,60 @@ describe QuestionsController do
   end
 
   context "EDIT" do
+   it "edit is successful" do
+      get :edit, id: @question.id
+      expect(response.status).to eq 200
+    end
 
+    it "renders the new template" do
+      get :edit, id: @question.id
+      expect(response).to render_template :edit, id: @question.id
+    end
+
+    it "routes /questions/:id/edit to questions#edit" do
+      expect(get: "/questions/#{@question.id}/edit").to route_to(
+        controller: "questions",
+        action: "edit", 
+        id: "#{@question.id}"
+        )
+    end
   end
 
   context "UPDATE" do
+    let(:no_title_question){ patch :update, id: @question, question: {title: "", content: @question.content} }
+    let(:question){ patch :update, id: @question, question: {title: "Hello", content: "This is a test"} }
+    
+    it "redirects to question#show if update is successful" do
+      expect(question).to redirect_to :action => :show,
+                                      :id => assigns(:question).id
+    end
 
+    it "renders the edit screen if the update doesn't save" do
+      expect(no_title_question).to render_template :edit
+    end
+
+    it "update is successful" do 
+      expect(response).to be_success
+    end
+
+    it "locates the requested question" do
+      patch :update, id: @question, question: {title: @question.title, content: @question.content}
+      expect(assigns(:question)).to eq(@question)
+    end
+
+    it "updates the request" do
+      patch :update, id: @question, question: {title:"Hello", content: @question.content}
+      @question.reload
+      expect(@question.title).to eq("Hello")
+    end
   end
 
   context "DESTROY" do
-
+    it "deletes the question" do
+      expect{ 
+        delete :destroy, :id => @question.id
+      }.to change(Question, :count).by(-1)
+    end
   end
 
   after(:each) do
